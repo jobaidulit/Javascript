82d5d1f7d89f7c50b20aa36b5acf0201e5236cc2
jobaidulit <jobaidulit@gmail.com>
2026-02-05 22:33:42 +0600

    Condition 1

--- condition.js
+++ condition.js
@@ -1,5 +1,9 @@
+// 6 am to 12pm "Good morning"
+// 12 pm to 6pm "Good Afternoon"
+// Otherwise "Good Evening"
 
-let time = 22;
+
+/* let time = 22;
 if (time >= 6 && time <12){
     console.log('Good Morning');
 }
@@ -8,4 +12,14 @@ else if(time >= 12 && time <2){
 }
 else{
     console.log("Good Evening");
+}
+    */
+
+let sajuSon = 'sani';
+let sajuDotor = 'safiya';
+if(sajuSon == 'sani' && sajuDotor == 'safiya'){
+    console.log('saju baba');
+}
+else{
+    console.log("not baba")
 }
