385b8788d88cf0e66f00c7b72f82a080e3122392
jobaidulit <jobaidulit@gmail.com>
2026-02-05 22:09:57 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,11 @@
+
+let time = 22;
+if (time >= 6 && time <12){
+    console.log('Good Morning');
+}
+else if(time >= 12 && time <2){
+    console.log('Good Afernoon');
+}
+else{
+    console.log("Good Evening");
+}
